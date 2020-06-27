e6f40135206ff052e814f1b1b5f9750f5ea795cf
Adriein <adria.claret@gmail.com>
2020-06-27 19:32:18 +0200

    fix for production env

--- src/index.ts
+++ src/index.ts
@@ -41,12 +41,11 @@ const init = async () => {
   app.use('/api/auth', auth);
   app.use('/api/admin', quotes);
   app.use(errorHandler);
-
   if (process.env.NODE_ENV === 'pro') {
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
     });
   }
 
